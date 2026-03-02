f0c95e3c3c77a3e4cbb281a85267ead9cd8cc696
dramhex <92268356+dramhex@users.noreply.github.com>
2026-03-02 03:11:17 +0100

    bugemon class and basic constructor

--- bugemon.py
+++ bugemon.py
@@ -1,4 +1,10 @@
-
-
 class Bugemon:
-    def __init__(self, name, sprite, )
+    def __init__(self, name , elementary_type : str, health, attack, defense, initiative, level = 1):
+        self.name = name
+        self.elementary_type = elementary_type
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+        self.initiative = initiative
+        self.level = level
+        self.experience = 0
