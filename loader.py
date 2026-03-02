f27a7ea03e4e7ac6697c9cb914014346a8b4960c
dramhex <92268356+dramhex@users.noreply.github.com>
2026-03-02 07:46:24 +0100

    loader update to load all attacks

--- loader.py
+++ loader.py
@@ -1,7 +1,14 @@
-from bugemon import Bugemon
+import json
 
-def load_bugemon(data: dict) -> Bugemon:
-    return Bugemon(
+from bugemon import Bugemon
+from ability import Ability
+
+def load_bugemon(data: dict, abilities: dict[str, Ability]) -> Bugemon:    
+    ability_set = []
+    for ability_id in data["attaques"]:
+        ability_set.append(abilities[ability_id])
+    
+    return Bugemon(    
         data["id"], 
         data["nom"], 
         data["type"], 
@@ -9,5 +16,26 @@ def load_bugemon(data: dict) -> Bugemon:
         data["stats"]["attaque"], 
         data["stats"]["defense"], 
         data["stats"]["initiative"],
-        []
-        )
+        ability_set
+    )
+    
+def load_ability(data: dict) -> Ability:
+    return Ability(
+        data["id"],
+        data["nom"],
+        data["description"],
+        data["type"],
+        data["puissance"],
+        data["effets"][0]
+    )
+    
+def load_all_abilities(filepath: str) -> dict[str, Ability]:
+    with open(filepath, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    abilities = {}
+    for ability_data in data["attaques"]:
+        ability = load_ability(ability_data)
+        abilities[ability.identifier] = ability
+    
+    return abilities
