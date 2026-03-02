83f883252041fa0fbc81dcb486333b3f415543a9
dramhex <92268356+dramhex@users.noreply.github.com>
2026-03-02 03:46:33 +0100

    start stat class

--- bugemon.py
+++ bugemon.py
@@ -1,10 +1,13 @@
+from bugemon_stat import Stat
+
 class Bugemon:
-    def __init__(self, name , elementary_type : str, health, attack, defense, initiative, level = 1):
+    def __init__(self, name , elementary_type, base_health, attack, defense, initiative, level = 1):
         self.name = name
         self.elementary_type = elementary_type
-        self.health = health
-        self.attack = attack
-        self.defense = defense
-        self.initiative = initiative
+        self.base_health = base_health
+        self.current_health = base_health
+        self.attack = Stat(attack)
+        self.defense = Stat(defense)
+        self.initiative = Stat(initiative)
         self.level = level
         self.experience = 0
---
+++ bugemon_stat.py
@@ -0,0 +1,10 @@
+class Stat:
+    def __init__(self, base_value):
+        self.base_value = base_value
+        self.current_value = base_value
+
+    def reset_current_value(self):
+        self.current_value = self.base_value
+
+    def apply_modificator(self, modificator):
+        self.current_value = max(0, self.current_value + modificator)
